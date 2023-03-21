e1189bd52b37f56efa3c3f7bcedd61e91374916a
imeuro <mauro.fioravanzi@gmail.com>
2023-03-21 19:23:00 +0100

    starter theme

---
+++ wp-content/themes/caw_2023/footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package caw_2023
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			&copy; 2023 - Cremona Contemporanea Art Week
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				// printf( esc_html__( 'Theme: %1$s by %2$s.', 'caw_2023' ), 'caw_2023', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
