1766b11649b163a0308a7e0f2ee7b57a499d2f53
imeuro <mauro.fioravanzi@gmail.com>
2023-04-13 00:47:55 +0200

    fix bug markers

--- wp-content/themes/caw_2023/home.php
+++ wp-content/themes/caw_2023/home.php
@@ -9,14 +9,13 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 		?>
-			<div id='caw-mapbox'>Loading map...</div>
+			
+			<div id='caw-mapbox'></div>
 			<div id="caw-content">
 				<div class="close-tabcontainer"></div>
 				<div id="caw-tabcontainer">Loading content...</div>
 			</div>
 
-			<!-- <iframe width='100%' height='400px' src="https://api.mapbox.com/styles/v1/meuro/clftwthuu002b01ogx6r445s7.html?title=false&access_token=pk.eyJ1IjoibWV1cm8iLCJhIjoiY2xmcjA2ZDczMDEwYTQzcWZwZXk4dmpvdSJ9.YHkGCdl-D6YkWDJbNGOBEQ&zoomwheel=false#13.64/45.13642/10.02768" title="CAW-2023" style="border:none;"></iframe> -->
-		
 		<?php
 		endwhile; // End of the loop.
 		?>
