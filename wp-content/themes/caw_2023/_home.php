4a107ea8b57d0769210963d3e790332383418560
imeuro <mauro.fioravanzi@gmail.com>
2023-04-26 15:19:26 +0200

    mod varie

---
+++ wp-content/themes/caw_2023/_home.php
@@ -0,0 +1,27 @@
+<?php /* Template Name: Opening Page */ 
+
+get_header();
+?>
+
+	<main id="primary" class="site-main site-main-home">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+		?>
+			
+			<div id='caw-mapbox'></div>
+			<div id="caw-content">
+				<div class="close-tabcontainer"></div>
+				<div id="caw-tabcontainer">Loading content...</div>
+			</div>
+
+		<?php
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
