9a9b6e68b08fb529c0b1f7d87e3c91cd1306038e
imeuro <mauro.fioravanzi@gmail.com>
2023-05-05 01:48:54 +0200

    speed tweaks

--- wp-content/themes/caw_2023/functions.php
+++ wp-content/themes/caw_2023/functions.php
@@ -140,3 +140,38 @@ require get_template_directory() . '/inc/template-functions.php';
 
 
 
+/**
+ * Disable the emoji's
+ */
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	
+	// Remove from TinyMCE
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+ * Filter out the tinymce emoji plugin.
+ */
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+
+function remove_wp_stupid_css(){
+ wp_dequeue_style( 'wp-block-library' );
+ wp_dequeue_style( 'wp-block-library-theme' );
+ wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+ wp_dequeue_style( 'classic-theme-styles' );
+} 
+add_action( 'wp_enqueue_scripts', 'remove_wp_stupid_css', 20 );
