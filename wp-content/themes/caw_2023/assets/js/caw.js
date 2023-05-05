25d8a02fbd38560c4d70262e70d120825fbf8dad
imeuro <mauro.fioravanzi@gmail.com>
2023-05-05 10:40:38 +0200

    better eventi

--- wp-content/themes/caw_2023/assets/js/caw.js
+++ wp-content/themes/caw_2023/assets/js/caw.js
@@ -344,7 +344,7 @@ const LoadItInTheDiv = (itemID, postType, divType, lang) => {
 					    el.acf.testo_eng.indexOf("<h3>") + 4, 
 					    el.acf.testo_eng.lastIndexOf("</h3>")
 					);
-					let engtext = el.acf.testo_eng.replace(engtitle,'');
+					let engtext = '<p>'+el.acf.testo_eng.replace(engtitle,'')+'</p>';
 
 					let event_content = current_lang == 'en' ? engtext : el.content.rendered;
 					let event_title = current_lang == 'en' ? engtitle : el.title.rendered;
