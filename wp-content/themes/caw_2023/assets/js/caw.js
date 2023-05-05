d8d164d1adf695af9360199cd21e9ec8fb66ec37
imeuro <mauro.fioravanzi@gmail.com>
2023-05-05 19:06:22 +0200

    varie

--- wp-content/themes/caw_2023/assets/js/caw.js
+++ wp-content/themes/caw_2023/assets/js/caw.js
@@ -482,12 +482,11 @@ const LoadItInTheDiv = (itemID, postType, divType, lang) => {
 							for (let i = 0; i < art_display.length; i++) {
 							TabContent += `
 								<div class="content-tabcontent content-artdisplay" id="artist-${art_display[i].slug}">
-									<h2>${art_display[i].title}</h3>
 									<div>${art_display[i].content}</div>
 								</div>`;
 							}
 							`<div class="content-tabcontent content-artdisplay">
-								${content_tabcontent}
+								<strong>${art_display[i].title}</strong> ${content_tabcontent}
 							</div>
 						`;
 						TabContainer.innerHTML = TabContent;
