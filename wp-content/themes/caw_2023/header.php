9743ad505ee8da4efad9c224b6e0eb3db2f26905
imeuro <mauro.fioravanzi@gmail.com>
2024-01-26 17:14:02 +0100

    2024 color: #1c39bb

--- wp-content/themes/caw_2023/header.php
+++ wp-content/themes/caw_2023/header.php
@@ -19,12 +19,12 @@
 
 
 	<link rel="icon" href="/favicon.ico">
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/apple-touch-icon.png?v=1.0">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/favicon-32x32.png?v=1.0">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/favicon-16x16.png?v=1.0">
-	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/site.webmanifest?v=1.0">
-	<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/safari-pinned-tab.svg?v=1.0" color="#ff0000">
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/favicon.ico?v=1.0">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/apple-touch-icon.png?v=2024">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/favicon-32x32.png?v=2024">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/favicon-16x16.png?v=2024">
+	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/site.webmanifest?v=2024">
+	<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/safari-pinned-tab.svg?v=2024" color="#ff0000">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/graphics/favicons/favicon.ico?v=2024">
 	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="theme-color" content="#ffffff">
 
